79dac759c4c92b9213d6f2d4d74da6b9310714f7
Scott Smith <mail@ScottHSmith.com>
2013-01-15 11:52:42 -0600

    Switched to jQuery 1.9 on Media Temple's CDN

--- header.php
+++ header.php
@@ -36,7 +36,7 @@ window.addEventListener("load",function() {
 });
 </script>
 
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="http://code.jquery.com/jquery-1.9.0.js"></script>
 
 <script type="text/javascript">
 	$(document).ready(function(){
