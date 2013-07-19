eafca2b87425b25f1aac75f0e5d0e0766088c374
Scott Smith <mail@ScottHSmith.com>
2013-07-19 02:22:57 -0500

    Theme info is displayed by default
    What? I worked hard on this!

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
 	</div><!-- #main -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="theme-info <?php echo get_theme_mod( 'show_theme_info', 'hidden' );?>">
+		<div class="theme-info <?php echo get_theme_mod( 'show_theme_info', '' );?>">
 			<p><a href="http://ScottHSmith.com/projects/decode/">Decode</a> by Scott Smith</p>
 		</div><!-- .theme-info -->
 	</footer><!-- #colophon -->
