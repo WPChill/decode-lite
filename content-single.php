e96c1039938cd7b9f4e49ad7ed4ac51b5bb33d49
Scott Smith <mail@ScottHSmith.com>
2013-07-19 00:40:56 -0500

    Corrected function name

--- content-single.php
+++ content-single.php
@@ -19,7 +19,7 @@
 	<?php elseif ( has_post_format( 'link' )): ?>
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<header class="entry-header">
-					<div class="entry-title"><h2><?php print_post_title() ?></h2>
+					<div class="entry-title"><h2><?php decode_print_post_title() ?></h2>
 					<div class="social-reply <?php echo get_theme_mod( 'show_reply_option', '' ); ?>"><a href="https://twitter.com/intent/tweet?screen_name=<?php echo get_theme_mod( 'twitter_reply_username', '' ); ?>&text=(about%3A%20<?php the_permalink(); ?>) " class="twitterreply replylink left" target="_blank" data-related="<?php echo get_theme_mod( 'twitter_reply_username', '' ); ?>">With Twitter</a><div class="replytrigger">Reply</div><a href="https://alpha.app.net/intent/post?text=@<?php echo get_theme_mod( 'adn_reply_username', '' ); ?> (about%3A%20<?php the_permalink(); ?>) " class="adnreply replylink right" target="_blank">With ADN</a></div></div>
 				</header>
 					<div class="entry-content"><?php the_content( __( 'continue reading &raquo;', 'twentyten' ) ); ?></div>
