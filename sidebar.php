b28e5952bba03fc9aa6431d4935a58f0d1beef18
Scott Smith <mail@ScottHSmith.com>
2013-07-19 02:20:32 -0500

    Removed meta widget from sidebar defaults

--- sidebar.php
+++ sidebar.php
@@ -20,14 +20,5 @@
 					</ul>
 				</aside>
 
-				<aside id="meta" class="widget">
-					<h1 class="widget-title"><?php _e( 'Meta', 'decode' ); ?></h1>
-					<ul>
-						<?php wp_register(); ?>
-						<li><?php wp_loginout(); ?></li>
-						<?php wp_meta(); ?>
-					</ul>
-				</aside>
-
 			<?php endif; // end sidebar widget area ?>
 		</div><!-- #secondary .widget-area -->
