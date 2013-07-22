464e420c2379d60cccf68fe873fc7a3d32792011
Scott Smith <mail@ScottHSmith.com>
2013-07-22 03:40:16 -0500

    Removed Reply Tool PHP, Improved display of search results

--- content.php
+++ content.php
@@ -19,8 +19,7 @@
 	<?php elseif ( has_post_format( 'link' )): ?>
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<header class="entry-header">
-					<div class="entry-title"><h2><?php decode_print_post_title() ?></h2>
-					<div class="social-reply <?php echo get_theme_mod( 'show_reply_option', 'hidden' ); ?>"><a href="https://twitter.com/intent/tweet?screen_name=<?php echo get_theme_mod( 'twitter_reply_username', '' ); ?>&text=(about%3A%20<?php the_permalink(); ?>) " class="twitterreply replylink left" target="_blank" data-related="<?php echo get_theme_mod( 'twitter_reply_username', '' ); ?>">With Twitter</a><div class="replytrigger">Reply</div><a href="https://alpha.app.net/intent/post?text=@<?php echo get_theme_mod( 'adn_reply_username' ); ?> (about%3A%20<?php the_permalink(); ?>) " class="adnreply replylink right" target="_blank">With ADN</a></div></div>
+					<div class="entry-title"><h2><?php decode_print_post_title() ?></h2></div>
 				</header>
 				
 					<div class="entry-content"><?php the_content( __( 'continue reading &raquo;', 'twentyten' ) ); ?></div>
@@ -30,21 +29,23 @@
 		<?php endif; ?>
 						<p class="date"><a href="<?php the_permalink(); ?>">Posted on <?php decode_posted_on(); ?></a></p>
 					</footer><!-- .entry-meta -->
-				</article>
+				</article><!-- #post-<?php the_ID(); ?> -->
 	
 	<?php elseif ( is_search() ) : // Only display Excerpts for Search ?>
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<div class="entry-summary">
-			<?php the_excerpt(); ?>
-		</div><!-- .entry-summary -->
-		<div class="entry-content">
-		</div>
+		<a href="<?php the_permalink(); ?>">
+			<div class="entry-summary">
+				<?php the_excerpt(); ?>
+			</div><!-- .entry-summary -->
+			<div class="entry-content">
+			</div>
+		</a>
+	</article>
 	
 	<?php else : ?>
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<div class="entry-title"><h2><?php the_title(); ?></h2>
-			<div class="social-reply <?php echo get_theme_mod( 'show_reply_option', 'hidden' ); ?>"><a href="https://twitter.com/intent/tweet?screen_name=<?php echo get_theme_mod( 'twitter_reply_username', '' ); ?>&text=(about%3A%20<?php the_permalink(); ?>) " class="twitterreply replylink left" target="_blank" data-related="<?php echo get_theme_mod( 'twitter_reply_username', '' ); ?>">With Twitter</a><div class="replytrigger">Reply</div><a href="https://alpha.app.net/intent/post?text=@<?php echo get_theme_mod( 'adn_reply_username', '' ); ?> (about%3A%20<?php the_permalink(); ?>) " class="adnreply replylink right" target="_blank">With ADN</a></div></div>
+		<div class="entry-title"><h2><?php the_title(); ?></h2></div>
 	</header><!-- .entry-header -->
 
 		<div class="entry-content"><?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'decode' ) ); ?></div>
