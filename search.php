62e0e98bd1b2c066749683360c1200874e7759a8
Scott Smith <mail@ScottHSmith.com>
2013-07-22 03:39:27 -0500

    Removed page forward/backward options at top of page

--- search.php
+++ search.php
@@ -16,8 +16,6 @@ get_header(); ?>
 					<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'decode' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 				</header><!-- .page-header -->
 
-				<?php decode_content_nav( 'nav-above' ); ?>
-
 				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 
