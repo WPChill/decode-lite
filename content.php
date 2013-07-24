d4ee3facef0a2d94764e1ffb87d19baf4a1bea8e
Scott Smith <mail@ScottHSmith.com>
2013-07-23 22:15:00 -0500

    Added featured image support

--- content.php
+++ content.php
@@ -23,7 +23,7 @@
 		<header class="entry-header">
 			<div class="entry-title"><h2><?php decode_print_post_title() ?></h2></div>
 		</header>
-		<div class="entry-content"><?php the_content( __( 'continue reading &raquo;', 'twentyten' ) ); ?></div>
+		<div class="entry-content"><?php the_content( __( 'continue reading &raquo;', 'decode' ) ); ?></div>
 		<footer class="entry-meta">
 			<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
 			<span class="comments-link <?php echo get_theme_mod( 'enable_comments', '' ); ?>"><?php comments_popup_link( __( 'Leave a comment', '_s' ), __( '1 Comment', '_s' ), __( '% Comments', '_s' ) ); ?></span>
@@ -31,6 +31,23 @@
 			<p class="date"><a href="<?php the_permalink(); ?>">Posted on <?php decode_posted_on(); ?></a></p>
 		</footer><!-- .entry-meta -->
 	</article><!-- #post-<?php the_ID(); ?> -->
+	
+<?php elseif ( has_post_thumbnail() && !is_search() ): ?>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<header class="entry-header">
+			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
+				<?php the_post_thumbnail(); ?>
+			</a>
+			<div class="entry-title"><h2><?php the_title(); ?></h2></div>
+		</header>
+		<div class="entry-content"><?php the_content( __( 'continue reading &raquo;', 'decode' ) ); ?></div>
+		<footer class="entry-meta">
+			<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+			<span class="comments-link <?php echo get_theme_mod( 'enable_comments', '' ); ?>"><?php comments_popup_link( __( 'Leave a comment', '_s' ), __( '1 Comment', '_s' ), __( '% Comments', '_s' ) ); ?></span>
+			<?php endif; ?>
+			<p class="date"><a href="<?php the_permalink(); ?>">Posted on <?php decode_posted_on(); ?></a></p>
+		</footer><!-- .entry-meta -->
+	</article>
 
 <?php elseif ( is_search() ) : // Only display Excerpts for Search ?>
 
