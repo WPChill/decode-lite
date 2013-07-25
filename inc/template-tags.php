7732479016ded786d5088ce4abb6f7c9c98f6964
Scott Smith <mail@ScottHSmith.com>
2013-07-24 21:12:40 -0500

    Reverted back to the old decode_posted_on() function

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -167,27 +167,12 @@ if ( ! function_exists( 'decode_posted_on' ) ) :
  * Prints HTML with meta information for the current post-date/time and author.
  */
 function decode_posted_on() {
-	$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) )
-		$time_string .= '<time class="updated" datetime="%3$s">%4$s</time>';
-
-	$time_string = sprintf( $time_string,
-		esc_attr( get_the_date( 'c' ) ),
-		esc_html( get_the_date() ),
-		esc_attr( get_the_modified_date( 'c' ) ),
-		esc_html( get_the_modified_date() )
-	);
-
-	printf( __( '<span class="posted-on">Posted on %1$s</span><span class="byline"> by %2$s</span>', 'decode' ),
-		sprintf( '<a href="%1$s" title="%2$s" rel="bookmark">%3$s</a>',
-			esc_url( get_permalink() ),
+	printf( __( '%2$s', 'decode' ),
+		'meta-prep meta-prep-author',
+		sprintf( '%3$s',
+			get_permalink(),
 			esc_attr( get_the_time() ),
-			$time_string
-		),
-		sprintf( '<span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s">%3$s</a></span>',
-			esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-			esc_attr( sprintf( __( 'View all posts by %s', 'decode' ), get_the_author() ) ),
-			esc_html( get_the_author() )
+			get_the_date()
 		)
 	);
 }
