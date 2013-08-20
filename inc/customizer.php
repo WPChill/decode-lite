348fca5ae4f4e2af64fa0acb67671862c90add16
Scott Smith <mail@ScottHSmith.com>
2013-08-20 08:38:13 -0500

    Register and enqueue script

--- inc/customizer.php
+++ inc/customizer.php
@@ -590,6 +590,7 @@ add_action( 'customize_register', 'decode_customize_register' );
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function decode_customize_preview_js() {
-	wp_enqueue_script( 'decode_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '2.7', true );
+	wp_register_script( 'decode-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview', 'jquery' ), '2.7', true );
+	wp_enqueue_script( 'decode-customizer' );
 }
 add_action( 'customize_preview_init', 'decode_customize_preview_js' );
