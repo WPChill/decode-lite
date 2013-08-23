1e6c89bc18aefa8769741dbb7753739bf27f0eec
Scott Smith <mail@ScottHSmith.com>
2013-08-23 18:03:44 -0500

    Bumped stylesheet version number to 2.7.3

--- functions.php
+++ functions.php
@@ -138,7 +138,7 @@ if ( ! is_admin() && ! function_exists( 'decode_scripts' ) ) {
 
 function decode_scripts() {
 
-	wp_register_style( 'decode-style', get_stylesheet_uri(), array(), "2.7.2" );
+	wp_register_style( 'decode-style', get_stylesheet_uri(), array(), "2.7.3" );
 
 	wp_register_style( 'decode-font-stylesheet', 'http://fonts.googleapis.com/css?family=Oxygen&subset=latin-ext' );
 	
