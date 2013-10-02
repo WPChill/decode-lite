0ac5b6733f81262fb463cf8cba49381bc5f83fc0
Scott Smith <mail@ScottHSmith.com>
2013-10-02 10:23:51 -0500

    Bumped version number to 2.8

--- functions.php
+++ functions.php
@@ -138,7 +138,7 @@ if ( ! is_admin() && ! function_exists( 'decode_scripts' ) ) {
 
 function decode_scripts() {
 
-	wp_enqueue_style( 'decode-style', get_stylesheet_uri(), array(), "2.7.6" );
+	wp_enqueue_style( 'decode-style', get_stylesheet_uri(), array(), "2.8" );
 
 	wp_enqueue_style( 'decode-font-stylesheet', '//fonts.googleapis.com/css?family=Oxygen&subset=latin-ext' );
 
