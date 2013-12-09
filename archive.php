5887cd296e6c0911fe16fcab804c1f447c4abfd7
Scott Smith <mail@ScottHSmith.com>
2013-12-08 23:30:25 -0600

    Made archive date formats translatable

--- archive.php
+++ archive.php
@@ -27,10 +27,10 @@ get_header(); ?>
 							printf( __( 'Day: %s', 'decode' ), '<span>' . get_the_date() . '</span>' );
 
 						elseif ( is_month() ) :
-							printf( __( 'Month: %s', 'decode' ), '<span>' . get_the_date( 'F Y' ) . '</span>' );
+							printf( __( 'Month: %s', '_s' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'decode' ) ) . '</span>' );
 
 						elseif ( is_year() ) :
-							printf( __( 'Year: %s', 'decode' ), '<span>' . get_the_date( 'Y' ) . '</span>' );
+							printf( __( 'Year: %s', 'decode' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'decode' ) ) . '</span>' );
 
 						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
 							_e( 'Asides', 'decode' );
