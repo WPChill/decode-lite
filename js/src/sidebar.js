5fa8e6a4ef2f8af0982d04eff1715eccd59a070b
Scott Smith <mail@ScottHSmith.com>
2014-01-13 23:29:45 -0600

    Added *helpful* comments

--- js/src/sidebar.js
+++ js/src/sidebar.js
@@ -1,4 +1,4 @@
-// Just a nice function to toggle classes
+// Just a nice function to toggle classes. Could I use new JavaScript features? Yes. Am I going to? No, keeping at least IE 9 support for this feature.
 function toggleClass(element, className){
     if (!element || !className){
         return;
@@ -19,6 +19,7 @@ window.addEventListener('load', function() {
     FastClick.attach(document.body);
 }, false);
 
+// Where the magic happens
 [].forEach.call( document.querySelectorAll("#sidebar_link, #sidebar_top"), function(el) {
 	el.addEventListener('click', function() {
 		toggleClass(document.getElementById('sidebar'), 'visible');
