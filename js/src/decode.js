ff5bcb471c6e023fcab96c43d836bc9146dc3d2e
Scott Smith <mail@ScottHSmith.com>
2014-01-14 10:05:30 -0600

    I accidentally was loading respond.js on all pages

--- js/src/decode.js
+++ js/src/decode.js
@@ -1,7 +1,9 @@
-Modernizr.load({
+Modernizr.load([{
 	test: Modernizr.mq('only all'),
-	nope: templateDir + '/js/respond.js?1.4.2',
+	nope: templateDir + '/js/respond.js?1.4.2'
+	},
 	
+	{
 	test: Modernizr.touch,
-	yep : templateDir + '/js/dropdown.js?3.0.3'
+	yep : templateDir + '/js/dropdown.js?3.0.3' }
 });
