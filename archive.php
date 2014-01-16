89ef8e05162d2b18d6ed8cecc23fb8e34d69759d
Scott Smith <mail@ScottHSmith.com>
2014-01-15 22:10:06 -0600

    Improved code formatting

--- archive.php
+++ archive.php
@@ -9,93 +9,94 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<section id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+	<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-						if ( is_category() ) :
-							single_cat_title();
-
-						elseif ( is_tag() ) :
-							single_tag_title();
-
-						elseif ( is_day() ) :
-							printf( __( 'Day: %s', 'decode' ), '<span>' . get_the_date() . '</span>' );
-
-						elseif ( is_month() ) :
-							printf( __( 'Month: %s', '_s' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'decode' ) ) . '</span>' );
-
-						elseif ( is_year() ) :
-							printf( __( 'Year: %s', 'decode' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'decode' ) ) . '</span>' );
-
-						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-							_e( 'Asides', 'decode' );
-
-						elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
-							_e( 'Galleries', 'decode');
-
-						elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-							_e( 'Images', 'decode');
-
-						elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-							_e( 'Videos', 'decode' );
-
-						elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-							_e( 'Quotes', 'decode' );
-
-						elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-							_e( 'Links', 'decode' );
-
-						elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
-							_e( 'Statuses', 'decode' );
-
-						elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
-							_e( 'Audios', 'decode' );
-
-						elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
-							_e( 'Chats', 'decode' );
-
-						else :
-							_e( 'Archives', 'decode' );
-
-						endif;
-					?>
-				</h1>
+		<header class="page-header">
+			<h1 class="page-title">
 				<?php
-					// Show an optional term description.
-					$term_description = term_description();
-					if ( ! empty( $term_description ) ) :
-						printf( '<div class="taxonomy-description">%s</div>', $term_description );
+					if ( is_category() ) :
+						single_cat_title();
+
+					elseif ( is_tag() ) :
+						single_tag_title();
+
+					elseif ( is_day() ) :
+						printf( __( 'Day: %s', 'decode' ), '<span>' . get_the_date() . '</span>' );
+
+					elseif ( is_month() ) :
+						printf( __( 'Month: %s', '_s' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'decode' ) ) . '</span>' );
+
+					elseif ( is_year() ) :
+						printf( __( 'Year: %s', 'decode' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'decode' ) ) . '</span>' );
+
+					elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
+						_e( 'Asides', 'decode' );
+
+					elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
+						_e( 'Galleries', 'decode');
+
+					elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
+						_e( 'Images', 'decode');
+
+					elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
+						_e( 'Videos', 'decode' );
+
+					elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
+						_e( 'Quotes', 'decode' );
+
+					elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
+						_e( 'Links', 'decode' );
+
+					elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
+						_e( 'Statuses', 'decode' );
+
+					elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
+						_e( 'Audios', 'decode' );
+
+					elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
+						_e( 'Chats', 'decode' );
+
+					else :
+						_e( 'Archives', 'decode' );
+
 					endif;
 				?>
-			</header><!-- .page-header -->
+			</h1>
+			<?php
+				// Show an optional term description.
+				$term_description = term_description();
+				if ( ! empty( $term_description ) ) :
+					printf( '<div class="taxonomy-description">%s</div>', $term_description );
+				endif;
+			?>
+		</header><!-- .page-header -->
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+		<?php /* Start the Loop */ ?>
+		<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to overload this in a child theme then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
+			<?php
+				/* Include the Post-Format-specific template for the content.
+				 * If you want to overload this in a child theme then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'content', get_post_format() );
+			?>
 
-			<?php endwhile; ?>
+		<?php endwhile; ?>
 
-			<?php decode_paging_nav(); ?>
+	<?php else : ?>
 
-		<?php else : ?>
+		<?php get_template_part( 'content-none', 'none' ); ?>
 
-			<?php get_template_part( 'content-none', 'none' ); ?>
+	<?php endif; ?>
 
-		<?php endif; ?>
-
-		</main><!-- #main -->
-	</section><!-- #primary -->
+	</main><!-- #main -->
+	
+	<?php decode_paging_nav(); ?>
+	
+</section><!-- #primary -->
 
 <?php get_footer(); ?>
--- index.php
+++ index.php
@@ -13,33 +13,34 @@
 
 get_header(); ?>
 
-		<section id="primary" class="content-area">
-			<main id="main" class="site-main" role="main">
+<section id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
 
-			<?php if ( have_posts() ) : ?>
+	<?php if ( have_posts() ) : ?>
 
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
+		<?php /* Start the Loop */ ?>
+		<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php
-						/* Include the Post-Format-specific template for the content.
-						 * If you want to overload this in a child theme then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'content', get_post_format() );
-					?>
+			<?php
+				/* Include the Post-Format-specific template for the content.
+				 * If you want to overload this in a child theme then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'content', get_post_format() );
+			?>
 
-				<?php endwhile; ?>
+		<?php endwhile; ?>
 
-			<?php else : ?>
+	<?php else : ?>
 
-				<?php get_template_part( 'content', 'none' ); ?>
+		<?php get_template_part( 'content', 'none' ); ?>
 
-			<?php endif; ?>
+	<?php endif; ?>
 
-			</main><!-- #main -->
-			
-			<?php decode_paging_nav(); ?>
-			
-		</section><!-- #primary -->
+	</main><!-- #main -->
+	
+	<?php decode_paging_nav(); ?>
+	
+</section><!-- #primary -->
+
 <?php get_footer(); ?>
