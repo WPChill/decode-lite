6c3ede8328ee2eb4bf2e1cac061d954fa674f812
Scott Smith <mail@ScottHSmith.com>
2014-01-22 16:08:38 -0600

    Getting there!

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,3 +1,4 @@
 
-[].forEach.call(document.querySelectorAll(".menu-item-has-children, .page_item_has_children"),function(element){element.firstChild.addEventListener('click',function(event){if(!(element.classList.contains('open'))){element.classList.toggle('open');event.preventDefault();}},false);});
+[].forEach.call(document.querySelectorAll(".menu-item-has-children, .page_item_has_children"),function(element){element.firstChild.addEventListener('click',function(event){if(document.querySelector('.open')&&!(element.parentElement.parentElement.classList.contains('open'))&&!(element.classList.contains('open'))){document.querySelector('.open').classList.remove('open');}
+if(!(element.classList.contains('open'))){element.classList.toggle('open');event.preventDefault();}},false);});
 //@ sourceMappingURL=srcmaps/dropdown.js.map
--- js/src/dropdown.js
+++ js/src/dropdown.js
@@ -1,10 +1,21 @@
+/*
+To Do:
+[X]  Only keep one drop down open in a parent-child group
+[X]  Opened links can be clicked
+[ ]  Hide dropdowns by a tap anywhere
+
+*/
 [].forEach.call( document.querySelectorAll(".menu-item-has-children, .page_item_has_children"), function(element) {
 	element.firstChild.addEventListener('click', function(event) {
+		
+		if ( document.querySelector('.open') && !(element.parentElement.parentElement.classList.contains('open')) && !(element.classList.contains('open')) ) {
+			document.querySelector('.open').classList.remove('open');
+		}
 		
 		if ( !(element.classList.contains('open')) ) {
 			element.classList.toggle('open');
 			event.preventDefault();
 		}
-		
+						
 	}, false);
 });
