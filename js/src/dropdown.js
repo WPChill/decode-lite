5e3db05a93c2584c62d06b8f9a64e571a28b05a5
Scott Smith <mail@ScottHSmith.com>
2014-01-22 17:19:39 -0600

    Dropdown.js is complete!

--- js/src/dropdown.js
+++ js/src/dropdown.js
@@ -1,17 +1,12 @@
-/*
-To Do:
-[X]  Only keep one drop down open in a parent-child group
-[X]  Opened links can be clicked
-[ ]  Hide dropdowns by a tap anywhere
-
-*/
 [].forEach.call( document.querySelectorAll(".menu-item-has-children, .page_item_has_children"), function(element) {
 	element.firstChild.addEventListener('click', function(event) {
 		
+		// Close other dropdowns unless they are a parent
 		if ( document.querySelector('.open') && !(element.parentElement.parentElement.classList.contains('open')) && !(element.classList.contains('open')) ) {
 			document.querySelector('.open').classList.remove('open');
 		}
 		
+		// Open the dropdown and don't open the link
 		if ( !(element.classList.contains('open')) ) {
 			element.classList.toggle('open');
 			event.preventDefault();
@@ -19,3 +14,25 @@ To Do:
 						
 	}, false);
 });
+
+// Alias all the names for the matches() method
+var matches;
+
+(function(doc) {
+	matches = 
+		doc.matches ||                // Latest Satandard
+		doc.matchesSelector ||        // Old Standard
+		doc.webkitMatchesSelector ||  // Safari 5+
+		doc.mozMatchesSelector ||     // Firefox 3.6+
+		doc.oMatchesSelector ||       // Opera 15+, 11.5+
+		doc.msMatchesSelector;        // IE 9+
+})(document.documentElement);
+
+// Dismiss dropdowns after a tap on any non-menu element
+if ( !(document.querySelector('.open')) ) {
+	document.addEventListener('click', function(event) {
+		if ( !(matches.call( event.target, '.menu *' ) ) ) {
+			document.querySelector('.open').classList.toggle('open');
+		}
+	}, false);
+}
