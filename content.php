e7db3debc9433797b21b2fcebd3950a19bb0a1d4
Scott Smith <mail@ScottHSmith.com>
2014-01-31 09:52:01 -0600

    Switch to only use one post excerpt template

--- content.php
+++ content.php
@@ -4,74 +4,40 @@
  */
 ?>
 
-<?php if ( get_theme_mod( 'use_excerpts', false ) == true && !is_sticky() ) : ?>
-
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<header class="entry-header">
-			<?php if ( has_post_thumbnail() && get_theme_mod( 'show_featured_images_on_excerpts', false ) == true ) : ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+			<?php if ( has_post_thumbnail() ) : ?>
 			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
 				<?php the_post_thumbnail(); ?>
 			</a>
 			<?php endif; ?>
-			<div class="entry-title"><h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2></div>
-			<?php if ( get_theme_mod( 'entry_date_position', 'below' ) == 'above' && get_theme_mod( 'show_entry_date_on_excerpts', false ) == true ) : ?>
-			<div class="entry-meta above-content">
-				<p class="date"><?php decode_posted_on(); ?></p>
-			</div><!-- .entry-meta -->
-			<?php endif; ?>
-		</header><!-- .entry-header -->
-		
-		<div class="entry-summary">
-			<?php the_excerpt(); ?>
-		</div><!-- .entry-summary -->
-		
-		<footer class="entry-meta">
-		<a class="read-more-link" href="<?php echo get_permalink(); ?>"><?php _e('Read More&hellip;', 'decode'); ?></a>
+		<div class="entry-title"><h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2></div>
+		<?php if ( get_theme_mod( 'entry_date_position', 'below' ) == 'above' ) : ?>
+		<div class="entry-meta above-content">
+			<p class="date"><?php decode_posted_on(); ?></p>
+		</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+	
+	<div class="entry-content">
+		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'decode' ) ); ?>
+	</div><!-- .entry-content -->
+	
+	<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'decode' ), 'after' => '</div>' ) ); ?>
+	
+	<footer class="entry-meta below-content">
+		<?php if (get_theme_mod( 'enable_comments', true ) == true && ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+			<div class="comments-link <?php echo get_theme_mod( 'enable_comments', '' ); ?>"><?php comments_popup_link( __( 'Leave a comment', 'decode' ), __( '1 Comment', 'decode' ), __( '% Comments', 'decode' ) ); ?></div>
+		<?php endif; ?>
 		<?php edit_post_link( __( 'Edit', 'decode' ), '<div class="edit-link">', '</div>' ); ?>
-		<?php if ( get_theme_mod( 'entry_date_position', 'below' ) == 'below' && get_theme_mod( 'show_entry_date_on_excerpts', false ) == true ) : ?>
-				<p class="date"><?php decode_posted_on(); ?></p>
-			<?php endif; ?>
-		</footer><!-- .entry-meta -->
-	</article><!-- #post-<?php the_ID(); ?> -->
-
-<?php else : ?>
-
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<header class="entry-header">
-				<?php if ( has_post_thumbnail() ) : ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
-					<?php the_post_thumbnail(); ?>
-				</a>
-				<?php endif; ?>
-			<div class="entry-title"><h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2></div>
-			<?php if ( get_theme_mod( 'entry_date_position', 'below' ) == 'above' ) : ?>
-			<div class="entry-meta above-content">
-				<p class="date"><?php decode_posted_on(); ?></p>
-			</div><!-- .entry-meta -->
-			<?php endif; ?>
-		</header><!-- .entry-header -->
-		
-		<div class="entry-content">
-			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'decode' ) ); ?>
-		</div><!-- .entry-content -->
-		
-		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'decode' ), 'after' => '</div>' ) ); ?>
-		
-		<footer class="entry-meta below-content">
-			<?php if (get_theme_mod( 'enable_comments', true ) == true && ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-				<div class="comments-link <?php echo get_theme_mod( 'enable_comments', '' ); ?>"><?php comments_popup_link( __( 'Leave a comment', 'decode' ), __( '1 Comment', 'decode' ), __( '% Comments', 'decode' ) ); ?></div>
-			<?php endif; ?>
-			<?php edit_post_link( __( 'Edit', 'decode' ), '<div class="edit-link">', '</div>' ); ?>
-			<?php if (get_theme_mod( 'show_tags', false ) == true ) : ?>
-				<p class="tags"><?php the_tags(__( 'Tagged as: ', 'decode' ),', '); ?></p>
-			<?php endif; ?>
-			<?php if (get_theme_mod( 'show_categories', false ) == true ) : ?>
-				<p class="categories"><?php _e( 'Categorized in&#58; ', 'decode' ) . the_category(', '); ?></p>
-			<?php endif; ?>
-			<?php if ( get_theme_mod( 'entry_date_position', 'below' ) == 'below' ) : ?>
-				<p class="date"><?php decode_posted_on(); ?></p>
-			<?php endif; ?>
-		</footer><!-- .entry-meta -->
-	</article><!-- #post-<?php the_ID(); ?> -->
-
-<?php endif; ?>
+		<?php if (get_theme_mod( 'show_tags', false ) == true ) : ?>
+			<p class="tags"><?php the_tags(__( 'Tagged as: ', 'decode' ),', '); ?></p>
+		<?php endif; ?>
+		<?php if (get_theme_mod( 'show_categories', false ) == true ) : ?>
+			<p class="categories"><?php _e( 'Categorized in&#58; ', 'decode' ) . the_category(', '); ?></p>
+		<?php endif; ?>
+		<?php if ( get_theme_mod( 'entry_date_position', 'below' ) == 'below' ) : ?>
+			<p class="date"><?php decode_posted_on(); ?></p>
+		<?php endif; ?>
+	</footer><!-- .entry-meta -->
+</article><!-- #post-<?php the_ID(); ?> -->
--- index.php
+++ index.php
@@ -22,11 +22,17 @@ get_header(); ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<?php
-				/* Include the Post-Format-specific template for the content.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'content', get_post_format() );
+				if ( get_theme_mod( 'use_excerpts', false ) == true && !is_sticky() ) {
+					get_template_part( 'content', 'excerpt' );
+				}
+				
+					/* Include the Post-Format-specific template for the content.
+					 * If you want to overload this in a child theme then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+				else {
+					get_template_part( 'content', get_post_format() );
+				}
 			?>
 
 		<?php endwhile; ?>
