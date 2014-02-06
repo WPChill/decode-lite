35fed005ee41e66e1100666252b31fb2d3aaf73c
Scott Smith <mail@ScottHSmith.com>
2014-02-06 10:01:52 -0600

    Spruced up image pages.

--- image.php
+++ image.php
@@ -19,12 +19,9 @@ get_header(); ?>
 				<div class="entry-meta">
 					<?php
 						$metadata = wp_get_attachment_metadata();
-						printf( __( 'Published <span class="entry-date"><time class="entry-date" datetime="%1$s">%2$s</time></span> at <a href="%3$s" title="Link to full-size image">%4$s &times; %5$s</a> in <a href="%6$s" title="Return to %7$s" rel="gallery">%8$s</a>', 'decode' ),
+						printf( __( 'Published <span class="entry-date"><time class="entry-date" datetime="%1$s">%2$s</time></span> in <a href="%3$s" title="Return to %4$s" rel="gallery">%5$s</a>', 'decode' ),
 							esc_attr( get_the_date( 'c' ) ),
 							esc_html( get_the_date() ),
-							esc_url( wp_get_attachment_url() ),
-							$metadata['width'],
-							$metadata['height'],
 							esc_url( get_permalink( $post->post_parent ) ),
 							esc_attr( strip_tags( get_the_title( $post->post_parent ) ) ),
 							get_the_title( $post->post_parent )
