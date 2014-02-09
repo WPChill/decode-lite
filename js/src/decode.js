48a4345c9894bac933f5973b5c940cf7a352c44a
Scott Smith <mail@ScottHSmith.com>
2014-02-09 17:38:35 -0600

    Minified dropdown script with Google Closure Complier

--- js/src/decode.js
+++ js/src/decode.js
@@ -5,78 +5,12 @@ Modernizr.load({
 
 if ( Modernizr.touch ) {
 	
-	var activeDropdownClass = 'open';
-	
-	// Select all .menu-item-has-children elements
-	[].forEach.call( document.querySelectorAll(".menu-item-has-children"), function(element) {
-		element.firstChild.addEventListener('click', function(event) {
-			
-			// Close other dropdowns unless they are a parent
-			if ( document.querySelector('.' + activeDropdownClass) && !(element.parentElement.parentElement.classList.contains(activeDropdownClass)) && !(element.classList.contains(activeDropdownClass)) ) {
-				// Iterate over each element with the active dropdown class and remove it
-				[].forEach.call( document.querySelectorAll('.' + activeDropdownClass), function(element) {
-					element.classList.remove(activeDropdownClass);
-				});
-			}
-			
-			// Open the dropdown and don't open the link
-			if ( !(element.classList.contains(activeDropdownClass)) ) {
-				element.classList.toggle(activeDropdownClass);
-				event.preventDefault();
-			}
-			
-			// Anything else and the link will open as normal
-							
-		}, false);
-	});
-	
-	// Select all .page_item_has_children elements
-	[].forEach.call( document.querySelectorAll(".page_item_has_children"), function(element) {
-		element.firstChild.addEventListener('click', function(event) {
-			
-			// Close other dropdowns unless they are a parent
-			if ( document.querySelector('.' + activeDropdownClass) && !(element.parentElement.parentElement.classList.contains(activeDropdownClass)) && !(element.classList.contains(activeDropdownClass)) ) {
-				// Iterate over each element with the active dropdown class and remove it
-				[].forEach.call( document.querySelectorAll('.' + activeDropdownClass), function(element) {
-					element.classList.remove(activeDropdownClass);
-				});
-			}
-			
-			// Open the dropdown and don't open the link
-			if ( !(element.classList.contains(activeDropdownClass)) ) {
-				element.classList.toggle(activeDropdownClass);
-				event.preventDefault();
-			}
-			
-			// Anything else and the link will open as normal
-							
-		}, false);
-	});
-	
-	// Alias all the names for the matches() method
-	var matches;
-	
-	(function(doc) {
-		matches = 
-			doc.matches ||                // Latest Satandard
-			doc.matchesSelector ||        // Old Standard
-			doc.webkitMatchesSelector ||  // Safari 5+
-			doc.mozMatchesSelector ||     // Firefox 3.6+
-			doc.oMatchesSelector ||       // Opera 15+, 11.5+
-			doc.msMatchesSelector;        // IE 9+
-	})(document.documentElement);
-	
-	// Dismiss dropdowns after a tap on any non-menu element
-	if ( !(document.querySelector('.' + activeDropdownClass)) ) {
-		document.addEventListener('click', function(event) {
-			if ( !(matches.call( event.target, '.menu-item' + '.' + activeDropdownClass + ' *' + ', .page_item' + '.' + activeDropdownClass + ' *' ) ) ) {
-				// Iterate over each element with the active dropdown class and remove it
-				[].forEach.call( document.querySelectorAll('.' + activeDropdownClass), function(element) {
-					element.classList.remove(activeDropdownClass);
-				});
-			}
-		}, false);
-	}	
+	var activeDropdownClass="open";
+	[].forEach.call(document.querySelectorAll(".menu-item-has-children"),function(a){a.firstChild.addEventListener("click",function(b){!document.querySelector("."+activeDropdownClass)||a.parentElement.parentElement.classList.contains(activeDropdownClass)||a.classList.contains(activeDropdownClass)||[].forEach.call(document.querySelectorAll("."+activeDropdownClass),function(a){a.classList.remove(activeDropdownClass)});a.classList.contains(activeDropdownClass)||(a.classList.toggle(activeDropdownClass),b.preventDefault())},
+	!1)});
+	[].forEach.call(document.querySelectorAll(".page_item_has_children"),function(a){a.firstChild.addEventListener("click",function(b){!document.querySelector("."+activeDropdownClass)||a.parentElement.parentElement.classList.contains(activeDropdownClass)||a.classList.contains(activeDropdownClass)||[].forEach.call(document.querySelectorAll("."+activeDropdownClass),function(a){a.classList.remove(activeDropdownClass)});a.classList.contains(activeDropdownClass)||(a.classList.toggle(activeDropdownClass),b.preventDefault())},
+	!1)});var matches;(function(a){matches=a.matches||a.matchesSelector||a.webkitMatchesSelector||a.mozMatchesSelector||a.oMatchesSelector||a.msMatchesSelector})(document.documentElement);
+	document.querySelector("."+activeDropdownClass)||document.addEventListener("click",function(a){matches.call(a.target,".menu-item."+activeDropdownClass+" *, .page_item."+activeDropdownClass+" *")||[].forEach.call(document.querySelectorAll("."+activeDropdownClass),function(a){a.classList.remove(activeDropdownClass)})},!1);
 
 }
 
