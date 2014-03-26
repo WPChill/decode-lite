bd208cff4fe34eaec6307a13d8ecc1f9c5533ca1
Scott Smith <mail@ScottHSmith.com>
2014-03-25 22:18:44 -0500

    Linked header image again

--- header.php
+++ header.php
@@ -52,7 +52,9 @@
 		<div class="site-branding">
 			
 			<?php if ( get_header_image() != '' ) : ?>
-				<img class="site-logo" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="100%" alt="" />
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+					<img class="site-logo" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="100%" alt="" />
+				</a>
 			<?php endif; ?>
 				
 			<?php if ( get_theme_mod( 'show_site_title', true ) == true ) : ?>			
