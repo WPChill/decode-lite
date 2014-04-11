82da6e2ddfcbe675076143f73071645e6ee53a9c
Scott Smith <mail@ScottHSmith.com>
2014-04-11 09:40:24 -0500

    Added description to sidebar

--- functions.php
+++ functions.php
@@ -127,6 +127,7 @@ function decode_widgets_init() {
 	register_sidebar( array(
 		'name'          => __( 'Sidebar', 'decode' ),
 		'id'            => 'sidebar-1',
+		'description'   => __( 'Only displayed if sidebar remains enabled in the Customize menu.', 'decode' ),
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
 	) );
