0760374d4202198fb448304d5dfd381e1bdd5d82
Scott Smith <mail@ScottHSmith.com>
2014-09-01 13:54:36 -0500

    Fixed Jetpack loading full posts, not excerpts

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -10,12 +10,26 @@
  * Add theme support for Infinite Scroll.
  * See: http://jetpack.me/support/infinite-scroll/
  */
+if ( ! function_exists( 'decode_render_infinte_scroll' ) ) {
+	
+function decode_render_infinte_scroll() {
+	while ( have_posts() ) : the_post();
+		if ( get_theme_mod( 'use_excerpts', false ) == true && ! is_sticky() ) :
+			get_template_part( 'content', 'excerpt' );
+		else :
+			get_template_part( 'content', get_post_format() );
+		endif;
+	endwhile;
+}
+}
+
 if ( ! function_exists( 'decode_jetpack_setup' ) ) {
 
 function decode_jetpack_setup() {
 	add_theme_support( 'infinite-scroll', array(
 		'container' => 'main',
-		'footer'    => 'page',
+		'footer'    => 'colophon',
+		'render'    => 'decode_render_infinte_scroll'
 	) );
 }
 }
