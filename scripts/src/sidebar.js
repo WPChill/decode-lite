a3b34d67505678f2f6c0dddb884bd18a8879cdd7
Scott Smith <mail@ScottHSmith.com>
2014-09-04 23:31:54 -0500

    Continue to refactor DecodeSidebar, props @derek-duncan

--- scripts/src/sidebar.js
+++ scripts/src/sidebar.js
@@ -1,40 +1,48 @@
 ( function () {
-	function DecodeSidebar( element, visibleclass ) {
-		visibleclass = visibleclass || 'sidebar-visible';
+	'use strict';
+	
+	function DecodeSidebar( elements, VisibleClass ) {
+		VisibleClass = VisibleClass || 'sidebar-visible';
 		
-		// If it's a modern browser:
-		if ( document.addEventListener ) {
-			document.getElementById( element ).addEventListener( 'click', function() {
-				toggleClass( document.body, 'sidebar-visible' );
-			}, false );
-		}
-		
-		// If it's IE 8 or some crap like that:
-		else if ( document.attachEvent )  {
-			document.getElementById( element ).attachEvent( 'onclick', function() {
-				toggleClass( document.body, 'sidebar-visible' );
-			} );
-		}
+		// Loop through the elements.
+		var i;
+		for ( i = 0; i < elements.length; i++ ) {
+			var element = elements[i];
 			
+			// If it's a modern browser:
+			if ( document.addEventListener ) {
+				document.getElementById( element ).addEventListener( 'click', toggleClass( document.body, VisibleClass ), false );
+			}
+			
+			// If it's IE 8 or some crap like that:
+			else if ( document.attachEvent )	 {
+				document.getElementById( element ).attachEvent( 'onclick', toggleClass( document.body, VisibleClass ) );
+			}
+		}
+		
 		// Just a nice function to toggle classes. Could I use new JavaScript features? Yes. Am I going to? No, keeping at least IE 9 support for this feature.
 		function toggleClass( element, className ) {
-		    if (!element || !className){
-		        return;
-		    }
-		    
-		    var classString = element.className, nameIndex = classString.indexOf(className);
-		    if (nameIndex === -1) {
-		        classString += ' ' + className;
-		    }
-		    else {
-		        classString = classString.substr(0, nameIndex) + classString.substr(nameIndex+className.length);
-		    }
-		    element.className = classString;
+			return function () {
+				if ( !element || !className ) {
+					return;
+				}
+				
+				var classString = element.className, nameIndex = classString.indexOf( className );
+				if ( nameIndex === -1 ) {
+					classString += ' ' + className;
+				}
+				
+				else {
+					classString = classString.substr( 0, nameIndex ) + classString.substr( nameIndex + className.length );
+				}
+			
+				element.className = classString;
+			};
 		}
 	}
 	
-	DecodeSidebar.init = function( element, visibleclass ) {
-		return new DecodeSidebar( element, visibleclass );
+	DecodeSidebar.init = function( elements, VisibleClass ) {
+		return new DecodeSidebar( elements, VisibleClass );
 	};
 	
 	if (typeof define == 'function' && typeof define.amd == 'object' && define.amd) {
