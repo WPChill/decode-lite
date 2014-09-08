41fc95354751d9adf8331a196e5f10939b38ed73
Scott Smith <mail@ScottHSmith.com>
2014-09-08 13:39:33 -0500

    Switched to Revealing Module Pattern

--- scripts/src/dropdown.js
+++ scripts/src/dropdown.js
@@ -1,57 +1,57 @@
-( function () {
+var DecodeDropdown = ( function () {
 	'use strict';
 	
 	function DecodeDropdown( parentElements, childElements, activeDropdownClass ) {
 		activeDropdownClass = activeDropdownClass || 'open';
 	
-	// Loop through the parentElements.
-	var i;
-	for ( i = 0; i < parentElements.length; i++ ) {
-		var element = parentElements[i];
-		
-		new OpenMenus( element, activeDropdownClass );
-		
-	}
-		
-	// Alias all the names for the matches() method.
-	var matches;
-	
-	( function( doc ) {
-		matches = 
-			doc.matches ||                // Latest Satandard
-			doc.matchesSelector ||        // Old Standard
-			doc.webkitMatchesSelector ||  // Safari 5+
-			doc.mozMatchesSelector ||     // Firefox 3.6+
-			doc.oMatchesSelector ||       // Opera 15+, 11.5+
-			doc.msMatchesSelector;        // IE 9+
-	} )( document.documentElement );
-	
-	// Dismiss dropdowns after a tap on any non-menu element
-	if ( ! ( document.querySelector( '.' + activeDropdownClass ) ) ) {
-		document.addEventListener( 'click', function( event ) {
+		// Loop through the parentElements.
+		var i;
+		for ( i = 0; i < parentElements.length; i++ ) {
+			var element = parentElements[i];
 			
-			// Loop through the childElements and output a string with a CSS selector for the open items.
-			var i;
-			var openItemSelector = '';
-			for ( i = 0; i < childElements.length; i++ ) {
-				var element = childElements[i];
+			new OpenMenus( element, activeDropdownClass );
+			
+		}
+			
+		// Alias all the names for the matches() method.
+		var matches;
+		
+		( function( doc ) {
+			matches = 
+				doc.matches ||                // Latest Satandard
+				doc.matchesSelector ||        // Old Standard
+				doc.webkitMatchesSelector ||  // Safari 5+
+				doc.mozMatchesSelector ||     // Firefox 3.6+
+				doc.oMatchesSelector ||       // Opera 15+, 11.5+
+				doc.msMatchesSelector;        // IE 9+
+		} )( document.documentElement );
+		
+		// Dismiss dropdowns after a tap on any non-menu element
+		if ( ! ( document.querySelector( '.' + activeDropdownClass ) ) ) {
+			document.addEventListener( 'click', function( event ) {
 				
-				// Only add a comma if this is the second or greater time through the loop.
-				if ( i > 0 ) {
-					openItemSelector = openItemSelector + ', .' + element + '.' + 'open' + ' *';
+				// Loop through the childElements and output a string with a CSS selector for the open items.
+				var i;
+				var openItemSelector = '';
+				for ( i = 0; i < childElements.length; i++ ) {
+					var element = childElements[i];
+					
+					// Only add a comma if this is the second or greater time through the loop.
+					if ( i > 0 ) {
+						openItemSelector = openItemSelector + ', .' + element + '.' + 'open' + ' *';
+					}
+					// Otherwise, don't add a comma.
+					else {
+						openItemSelector = openItemSelector + '.' + element + '.' + 'open' + ' *';
+					}
 				}
-				// Otherwise, don't add a comma.
-				else {
-					openItemSelector = openItemSelector + '.' + element + '.' + 'open' + ' *';
+				
+				if ( ! ( matches.call( event.target, openItemSelector ) ) ) {
+					new CloseMenus( activeDropdownClass );
 				}
-			}
-			
-			if ( ! ( matches.call( event.target, openItemSelector ) ) ) {
-				new CloseMenus( activeDropdownClass );
-			}
-			
-		}, false );
-	}
+				
+			}, false );
+		}
 	}
 	
 	// Handles opening menus and closing currently open menus when a new menu is selected.
@@ -83,22 +83,12 @@
 			element.classList.remove(activeDropdownClass);
 		} );
 	}
-
 	
-	DecodeDropdown.init = function( parentElements, childElements, activeDropdownClass ) {
+	function init( parentElements, childElements, activeDropdownClass ) {
 		return new DecodeDropdown( parentElements, childElements, activeDropdownClass );
-	};
-	
-	if ( typeof define == 'function' && typeof define.amd == 'object' && define.amd ) {
-	
-		// AMD. Register as an anonymous module.
-		define( function() {
-			return DecodeDropdown;
-		} );
-	} else if ( typeof module !== 'undefined' && module.exports ) {
-		module.exports = DecodeDropdown.init;
-		module.exports.DecodeDropdown = DecodeDropdown;
-	} else {
-		window.DecodeDropdown = DecodeDropdown;
 	}
+	
+	return {
+		init: init
+	};
 } )();
