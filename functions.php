234d76d37d9415e4c5a744d1591d40f032b8c5c1
Scott Smith <mail@ScottHSmith.com>
2014-10-18 14:20:17 -0700

    Prefix lone func, add string for l8n

--- functions.php
+++ functions.php
@@ -228,7 +228,7 @@ add_filter( 'body_class', 'decode_add_body_classes' );
  */
 function decode_add_google_profile( $contactmethods ) {
 	// Add Google Profiles
-	$contactmethods['google_profile'] = 'Google Profile URL';
+	$contactmethods['google_profile'] = __( 'Google Profile URL', 'decode' );
 	return $contactmethods;
 }
 add_filter( 'user_contactmethods', 'decode_add_google_profile', 10, 1 );
@@ -286,9 +286,9 @@ if ( ! function_exists( 'decode_print_post_title' ) ) {
 /*
  * Show all post types in main query
  */
-if ( ! function_exists( 'add_post_types_to_query' ) ) {
+if ( ! function_exists( 'decode_add_post_types_to_query' ) ) {
 
-function add_post_types_to_query( $query ) {
+function decode_add_post_types_to_query( $query ) {
 		$typelist = 'post';
 		if ( get_theme_mod( 'add_custom_post_types', '' ) !== '' ) {
 			$typelist .= ', ' . get_theme_mod( 'add_custom_post_types', '' );
@@ -301,5 +301,5 @@ function add_post_types_to_query( $query ) {
 }
 
 if ( get_theme_mod( 'add_custom_post_types', '' ) !== '' ) {
-	add_action( 'pre_get_posts', 'add_post_types_to_query' );
+	add_action( 'pre_get_posts', 'decode_add_post_types_to_query' );
 }
