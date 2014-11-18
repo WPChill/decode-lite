6e9931ae3ee12eb2aa9bb44b7e8c3320d4adb30c
Scott Smith <mail@ScottHSmith.com>
2014-11-17 22:57:52 -0800

    Simplify and improve archive page titles in WP 4.1

--- archive.php
+++ archive.php
@@ -9,68 +9,15 @@
 
 get_header(); ?>
 
-<section id="primary" class="content-area">
+<div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 
 	<?php if ( have_posts() ) : ?>
 
 		<header class="page-header">
-			<h1 class="page-title">
-				<?php
-					if ( is_category() ) :
-						single_cat_title();
-
-					elseif ( is_tag() ) :
-						single_tag_title();
-
-					elseif ( is_day() ) :
-						printf( __( 'Day: %s', 'decode' ), '<span>' . get_the_date() . '</span>' );
-
-					elseif ( is_month() ) :
-						printf( __( 'Month: %s', 'decode' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'decode' ) ) . '</span>' );
-
-					elseif ( is_year() ) :
-						printf( __( 'Year: %s', 'decode' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'decode' ) ) . '</span>' );
-
-					elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-						_e( 'Asides', 'decode' );
-
-					elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
-						_e( 'Galleries', 'decode' );
-
-					elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-						_e( 'Images', 'decode' );
-
-					elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-						_e( 'Videos', 'decode' );
-
-					elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-						_e( 'Quotes', 'decode' );
-
-					elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-						_e( 'Links', 'decode' );
-
-					elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
-						_e( 'Statuses', 'decode' );
-
-					elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
-						_e( 'Audios', 'decode' );
-
-					elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
-						_e( 'Chats', 'decode' );
-
-					else :
-						_e( 'Archives', 'decode' );
-
-					endif;
-				?>
-			</h1>
 			<?php
-				// Show an optional term description.
-				$term_description = term_description();
-				if ( ! empty( $term_description ) ) :
-					printf( '<div class="taxonomy-description">%s</div>', $term_description );
-				endif;
+				the_archive_title( '<h1 class="page-title">', '</h1>' );
++				the_archive_description( '<div class="taxonomy-description">', '</div>' );
 			?>
 		</header><!-- .page-header -->
 
@@ -101,6 +48,6 @@ get_header(); ?>
 	
 	<?php decode_paging_nav(); ?>
 	
-</section><!-- #primary -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
