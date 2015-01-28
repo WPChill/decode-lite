b7872ebf28a1635237da43437d82a952b0138f90
Scott Smith <mail@ScottHSmith.com>
2015-01-28 13:22:41 -0800

    Use core navigation template tags.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -9,7 +9,7 @@
  
 if ( ! function_exists( 'decode_srcset_post_thumbnail' ) ) :
 /**
- * Display navigation to next/previous set of posts when applicable.
+ * Filters post thumbnails/featured images to use src-set responive images spec.
  */
 function decode_srcset_post_thumbnail( $html, $post_id, $post_thumbnail_id, $size, $attr ) {
 	$full_image_url  = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), 'full' );
@@ -23,65 +23,6 @@ function decode_srcset_post_thumbnail( $html, $post_id, $post_thumbnail_id, $siz
 }
 endif;
 add_filter( 'post_thumbnail_html', 'decode_srcset_post_thumbnail', 10, 5 );
-
-if ( ! function_exists( 'decode_paging_nav' ) ) :
-/**
- * Display navigation to next/previous set of posts when applicable.
- *
- * @return void
- */
-function decode_paging_nav() {
-        // Don't print empty markup if there's only one page.
-        if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
-                return;
-        }
-        ?>
-        <nav class="navigation paging-navigation" role="navigation">
-                <h1 class="screen-reader-text"><?php _e( 'Posts navigation', 'decode' ); ?></h1>
-                <div class="nav-links">
-
-                        <?php if ( get_next_posts_link() ) : ?>
-                        <div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span>&nbsp;Older posts', 'decode' ) ); ?></div>
-                        <?php endif; ?>
-
-                        <?php if ( get_previous_posts_link() ) : ?>
-                        <div class="nav-next"><?php previous_posts_link( __( 'Newer posts&nbsp;<span class="meta-nav">&rarr;</span>', 'decode' ) ); ?></div>
-                        <?php endif; ?>
-
-                </div><!-- .nav-links -->
-        </nav><!-- .navigation -->
-        <?php
-}
-endif;
-
-if ( ! function_exists( 'decode_post_nav' ) ) :
-/**
- * Display navigation to next/previous post when applicable.
- *
- * @return void
- */
-function decode_post_nav() {
-        // Don't print empty markup if there's nowhere to navigate.
-        $previous = ( is_attachment() ) ? get_post( get_post()->post_parent ) : get_adjacent_post( false, '', true );
-        $next     = get_adjacent_post( false, '', false );
-
-        if ( ! $next && ! $previous ) {
-                return;
-        }
-        ?>
-        <nav class="navigation post-navigation" role="navigation">
-                <h1 class="screen-reader-text"><?php _e( 'Post navigation', 'decode' ); ?></h1>
-                <div class="nav-links">
-                	<?php
-						previous_post_link( '<div class="nav-previous">%link</div>', _x( '<span class="meta-nav">&larr;</span>&nbsp;%title', 'Previous post link', 'decode' ) );
-						next_post_link(     '<div class="nav-next">%link</div>',     _x( '%title&nbsp;<span class="meta-nav">&rarr;</span>', 'Next post link',     'decode' ) );
-					?>
-                </div><!-- .nav-links -->
-        </nav><!-- .navigation -->
-        <?php
-}
-endif;
-
 
 if ( ! function_exists( 'decode_author_section' ) ) :
 /**
@@ -198,6 +139,62 @@ function decode_posted_on() {
 
 	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>';
 
+}
+endif;
+
+if ( ! function_exists( 'the_posts_navigation' ) ) :
+/**
+ * Display navigation to next/previous set of posts when applicable.
+ *
+ * @todo Remove this function when WordPress 4.3 is released.
+ */
+function the_posts_navigation() {
+	// Don't print empty markup if there's only one page.
+	if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
+		return;
+	}
+	?>
+	<nav class="navigation posts-navigation" role="navigation">
+		<h2 class="screen-reader-text"><?php _e( 'Posts navigation', 'decode' ); ?></h2>
+		<div class="nav-links">
+
+			<?php if ( get_next_posts_link() ) : ?>
+			<div class="nav-previous"><?php next_posts_link( __( 'Older posts', 'decode' ) ); ?></div>
+			<?php endif; ?>
+
+			<?php if ( get_previous_posts_link() ) : ?>
+			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts', 'decode' ) ); ?></div>
+			<?php endif; ?>
+
+		</div><!-- .nav-links -->
+	</nav><!-- .navigation -->
+	<?php
+}
+endif;
+if ( ! function_exists( 'the_post_navigation' ) ) :
+/**
+ * Display navigation to next/previous post when applicable.
+ *
+ * @todo Remove this function when WordPress 4.3 is released.
+ */
+function the_post_navigation() {
+	// Don't print empty markup if there's nowhere to navigate.
+	$previous = ( is_attachment() ) ? get_post( get_post()->post_parent ) : get_adjacent_post( false, '', true );
+	$next     = get_adjacent_post( false, '', false );
+	if ( ! $next && ! $previous ) {
+		return;
+	}
+	?>
+	<nav class="navigation post-navigation" role="navigation">
+		<h2 class="screen-reader-text"><?php _e( 'Post navigation', 'decode' ); ?></h2>
+		<div class="nav-links">
+			<?php
+				previous_post_link( '<div class="nav-previous">%link</div>', '%title' );
+				next_post_link( '<div class="nav-next">%link</div>', '%title' );
+			?>
+		</div><!-- .nav-links -->
+	</nav><!-- .navigation -->
+	<?php
 }
 endif;
 
