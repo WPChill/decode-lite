313f3c7ea8aaa071ba70e886d7dd90128ddad9cd
Robciuc Ioan <robciucioan94@gmail.com>
2016-05-06 09:48:43 +0300

    Has been added related posts action.

--- single.php
+++ single.php
@@ -4,29 +4,20 @@
  *
  * @package Decode
  */
-
-get_header(); ?>
-
+?>
+<?php get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-
 	<?php while ( have_posts() ) : the_post(); ?>
-
 		<?php get_template_part( 'content', 'single' ); ?>
-		
-		 <?php
-		 		// If comments are open or we have at least one comment, load up the comment template.
-		 		if ( comments_open() || get_comments_number() ) {
-		 			comments_template();
-		 		}
+		<?php do_action( 'related_posts' ); ?>
+		<?php
+		if ( comments_open() || get_comments_number() ) {
+			comments_template();
+		}
 		 ?>
-
 	<?php endwhile; // end of the loop. ?>
-
 	</main><!-- #main -->
-	
 	<?php the_post_navigation(); ?>
-
 </div><!-- #primary -->
-
 <?php get_footer(); ?>
